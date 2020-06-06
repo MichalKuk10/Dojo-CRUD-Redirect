bba6ef26c3e6470241b23387355f9b9f2bc74032
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-06-06 15:55:51 +0200

    Add servlet methods

--- src/main/java/Controller/StudentController.java
+++ src/main/java/Controller/StudentController.java
@@ -1,6 +1,22 @@
 package Controller;
 
-public class StudentController {
+import Model.Student;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@WebServlet(name = "allStudents", urlPatterns = {"/students"})
+public class StudentController extends HttpServlet {
+    private List<Student> listOfStudents;
+
+
+    public StudentController(){
+        listOfStudents = new ArrayList<>();
+    }
+
     public void handle(){
 
     }
@@ -8,8 +24,9 @@ public class StudentController {
 
     }
 
-    private void add(int id){
-
+    private void add(int id, String firstName, String lastName, int age){
+        Student newStudent = new Student(id, firstName, lastName, age);
+        listOfStudents.add(newStudent);
     }
 
     private void edit(int id){
