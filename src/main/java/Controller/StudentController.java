16e7c4bc36e4c5f9886cc3973c55ed51b4620ca9
Michal Kuk <kuk.michal3@gmail.com>
2020-06-06 15:05:56 +0200

    initial commit

---
+++ src/main/java/Controller/StudentController.java
@@ -0,0 +1,22 @@
+package Controller;
+
+public class StudentController {
+    public void handle(){
+
+    }
+    private void index(){
+
+    }
+
+    private void add(int id){
+
+    }
+
+    private void edit(int id){
+
+    }
+
+    private void delete(int id){
+
+    }
+}
